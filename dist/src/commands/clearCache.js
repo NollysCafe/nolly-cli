c87c277848acf43da902a4ea40e6f2ee7820b8a3
TheNolle <github@thenolle.com>
2025-01-25 00:26:17 +0100

    publishing

--- dist/src/commands/clearCache.js
+++
@@ -1,7 +0,0 @@
-import { clearCache } from '../utils/cache.js';
-import chalk from 'chalk';
-export const handleClearCacheCommand = () => {
-    clearCache();
-    console.log(chalk.green('✅ Cache cleared successfully.'));
-};
-//# sourceMappingURL=clearCache.js.map
